1c6257bbd860f352817f46690f36815c0139d73b
Massimiliano Rinaudo <movipadel@gmail.com>
2026-02-16 16:26:30 +0100

    redirect vercel domain to custom domain

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "tornei-app.vercel.app",
+          },
+        ],
+        destination: "https://tornei.movipadel.it/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
